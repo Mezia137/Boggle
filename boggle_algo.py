a1d48cec463973bb34568d47fbc2ea8f7e416599
meziane <meziane@gostiaux.fr>
2023-05-12 10:02:28 +0200

    digit tree

--- boggle_algo.py
+++ boggle_algo.py
@@ -4,203 +4,219 @@ import random
 import numpy as np
 
 DES = [
-    ['L', 'E', 'N', 'U', 'Y', 'G'],
-    ['E', 'L', 'U', 'P', 'S', 'T'],
-    ['Z', 'D', 'V', 'N', 'E', 'A'],
-    ['S', 'D', 'T', 'N', 'O', 'E'],
-    ['A', 'M', 'O', 'R', 'I', 'S'],
-    ['F', 'X', 'R', 'A', 'O', 'I'],
-    ['M', 'O', 'Q', 'A', 'B', 'J'],
-    ['F', 'S', 'H', 'E', 'E', 'I'],
-    ['H', 'R', 'S', 'N', 'E', 'I'],
-    ['E', 'T', 'N', 'K', 'O', 'U'],
-    ['T', 'A', 'R', 'I', 'L', 'B'],
-    ['T', 'I', 'E', 'A', 'O', 'A'],
-    ['A', 'C', 'E', 'P', 'D', 'M'],
-    ['R', 'L', 'A', 'S', 'E', 'C'],
-    ['U', 'L', 'I', 'W', 'E', 'R'],
-    ['V', 'G', 'T', 'N', 'I', 'E']]
+	['L', 'E', 'N', 'U', 'Y', 'G'],
+	['E', 'L', 'U', 'P', 'S', 'T'],
+	['Z', 'D', 'V', 'N', 'E', 'A'],
+	['S', 'D', 'T', 'N', 'O', 'E'],
+	['A', 'M', 'O', 'R', 'I', 'S'],
+	['F', 'X', 'R', 'A', 'O', 'I'],
+	['M', 'O', 'Q', 'A', 'B', 'J'],
+	['F', 'S', 'H', 'E', 'E', 'I'],
+	['H', 'R', 'S', 'N', 'E', 'I'],
+	['E', 'T', 'N', 'K', 'O', 'U'],
+	['T', 'A', 'R', 'I', 'L', 'B'],
+	['T', 'I', 'E', 'A', 'O', 'A'],
+	['A', 'C', 'E', 'P', 'D', 'M'],
+	['R', 'L', 'A', 'S', 'E', 'C'],
+	['U', 'L', 'I', 'W', 'E', 'R'],
+	['V', 'G', 'T', 'N', 'I', 'E']]
 
 random.shuffle(DES)
 board = np.array([random.choice(de) for de in DES]).reshape((4, 4))
 for i in board:
-    print(i)
+	print(i)
 
 alpha = {'A': 0,
-         'B': 1,
-         'C': 2,
-         'D': 3,
-         'E': 4,
-         'F': 5,
-         'G': 6,
-         'H': 7,
-         'I': 8,
-         'J': 9,
-         'K': 10,
-         'L': 11,
-         'M': 12,
-         'N': 13,
-         'O': 14,
-         'P': 15,
-         'Q': 16,
-         'R': 17,
-         'S': 18,
-         'T': 19,
-         'U': 20,
-         'V': 21,
-         'W': 22,
-         'X': 23,
-         'Y': 24,
-         'Z': 25}
+		 'B': 1,
+		 'C': 2,
+		 'D': 3,
+		 'E': 4,
+		 'F': 5,
+		 'G': 6,
+		 'H': 7,
+		 'I': 8,
+		 'J': 9,
+		 'K': 10,
+		 'L': 11,
+		 'M': 12,
+		 'N': 13,
+		 'O': 14,
+		 'P': 15,
+		 'Q': 16,
+		 'R': 17,
+		 'S': 18,
+		 'T': 19,
+		 'U': 20,
+		 'V': 21,
+		 'W': 22,
+		 'X': 23,
+		 'Y': 24,
+		 'Z': 25}
 
 alphabet = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
 digit_board = [[alpha[board[row, i]] for i in range(len(board[row]))] for row in range(len(board))]
 for i in digit_board:
-    print(i)
+	print(i)
 
 class digit_tree():
-    def __init__(self, lettre_id, parent=None):
-        self.lettre_id = lettre_id
-        self.parent = parent
-        self.child = []
-        self.word = False
+	def __init__(self, lettre, parent=None):
+		self.lettre = lettre
+		self.parent = parent
+		self.child = [np.full(shape=25, fill_value=None)]
+		self.word = False
 
-
+	def get_word(self):
+		word = []
+		noeud = self
+		while noeud.parent:
+			word.insert(0, noeud.lettre)
+			noeud = noeud.parent
 
 class dict_object():
-    def __init__(self, lettre, parent=None):
-        self.lettre = lettre
-        self.word = False
-        self.child = []
-        self.parent = parent
+	def __init__(self, lettre, parent=None):
+		self.lettre = lettre
+		self.word = False
+		self.child = []
+		self.parent = parent
 
-    def get_word(self):
-        word = ''
-        noeud = self
-        while noeud.parent:
-            word = noeud.lettre + word
-            noeud = noeud.parent
+	def get_word(self):
+		word = ''
+		noeud = self
+		while noeud.parent:
+			word = noeud.lettre + word
+			noeud = noeud.parent
 
-        return word
+		return word
 
 
 def build_tree(input_file):
-    dictionnaire = dict_object('')
-    with open(input_file, 'r') as words:
-        for word in words:
-            page = dictionnaire
-            # print(word, end='')
-            for i, lettre in enumerate(word[:-1]):
-                # if lettre == "\n":
-                #   break
+	dictionnaire = dict_object('')
+	with open(input_file, 'r') as words:
+		for word in words:
+			page = dictionnaire
+			# print(word, end='')
+			for i, lettre in enumerate(word[:-1]):
+				# if lettre == "\n":
+				#   break
 
-                if not lettre in [p.lettre for p in page.child]:
-                    page.child.append(dict_object(lettre, parent=page))
-                    # print('ajoute', lettre)
+				if not lettre in [p.lettre for p in page.child]:
+					page.child.append(dict_object(lettre, parent=page))
+					# print('ajoute', lettre)
 
-                for p in page.child:
-                    # print(p.lettre)
-                    if lettre == p.lettre:
-                        # print('lettre found')
-                        page = p
-                        if word[i + 1] == "\n":
-                            p.word = True
-                            # print('word found')
-                        break
-    return dictionnaire
+				for p in page.child:
+					# print(p.lettre)
+					if lettre == p.lettre:
+						# print('lettre found')
+						page = p
+						if word[i + 1] == "\n":
+							p.word = True
+							# print('word found')
+						break
+	return dictionnaire
 
 def build_digit_tree(input_file):
-    tree = digit_tree(None)
-    with open(input_file, 'r') as words:
-        for word in words:
-            branch = tree
-            for i, lettre in enumerate(word[:-1]):
-                lettre_id = alpha[lettre]
-                if not lettre_id in [b.lettre_id for b in branch.child]:
-                    branch.child.append(digit_tree(lettre_id, parent=branch))
+	tree = digit_tree(None)
+	with open(input_file, 'r') as words:
+		for word in words:
+			print(word)
+			branch = tree	
+			for i, lettre in enumerate(word[:-1]):
+				print(lettre)
+				lettre_id = alpha[lettre]
+				#if not lettre_id in [b.lettre_id for b in branch.child]:
+				#if branch.child[lettre_id] == None:
+				if not isinstance(branch.child[lettre_id], np.ndarray):
+					branch.child[lettre_id] = digit_tree(lettre_id, parent=branch)
 
-                for b in branch.child:
-                    if lettre_id == b.lettre_id:
-                        branch = b
-                        if word[i+1]=='\n':
-                            b.word = True
+				# ~ for b in branch.child:
+					# ~ if lettre_id == b.lettre_id:
+						# ~ branch = b
+						# ~ if word[i+1]=='\n':
+							# ~ b.word = True
+							# ~ break
+				
+				
+				branch = branch.child[lettre_id]
+				if word[i+1]=='\n':
+					branch.word = True
 
-                        break
-
-    return tree
+	return tree
 
 
 def show_tree(noeud, indent=''):
-    print(indent + noeud.lettre, end='')
-    if noeud.word:
-        print(' -> ', noeud.get_word())
-    else:
-        print('')
+	print(indent + noeud.lettre, end='')
+	if noeud.word:
+		print(' -> ', noeud.get_word())
+	else:
+		print('')
 
-    for n in noeud.child:
-        show_tree(n, indent=indent + '  ')
+	for n in noeud.child:
+		show_tree(n, indent=indent + '  ')
 
 def show_digit_tree(noeud, indent=''):
-    print(indent + str(noeud.lettre_id), end='')
-    if noeud.word:
-        print(' -> ', noeud.get_word())
-    else:
-        print('')
+	print(indent + str(noeud.lettre_id), end='')
+	# ~ if noeud.word:
+		# ~ print(' -> ', noeud.get_word())
+	# ~ else:
+	print('')
 
-    for n in noeud.child:
-        show_digit_tree(n, indent=indent + '  ')
+	for n in noeud.child:
+		if isinstance(n, np.ndarray):
+			show_digit_tree(n, indent=indent + '  ')
 
-#dictionnaire = build_tree('words.txt')
 dictionnaire = build_digit_tree('words.txt')
 show_digit_tree(dictionnaire)
+
+#dictionnaire = build_tree('words.txt')
+#show_tree(dictionnaire)
 
 # à voir
 # arbre de patricia
 # arbre préfix   suffix
 
 def finder(key, tree):
-    for branche in tree.child:
-        if key == branche.lettre:
-            return branche
-    return False
+	for branche in tree.child:
+		if key == branche.lettre:
+			return branche
+	return False
 
 directions = [
-    (-1, 0),
-    (-1, 1),
-    (0, 1),
-    (1, 1),
-    (1, 0),
-    (1, -1),
-    (0, -1),
-    (-1, -1),
+	(-1, 0),
+	(-1, 1),
+	(0, 1),
+	(1, 1),
+	(1, 0),
+	(1, -1),
+	(0, -1),
+	(-1, -1),
 ]
 
 def parser(y, x, board, tree):
 
-    #tree = finder(board[(y, x)], tree)
-    #print(tree.lettre)
-    for d in directions:
-        if 0<=y+d[0]<=3 and 0<=x+d[1]<=3 and not board[y+d[0],x+d[1]] in passed:
-            y+=d[0]
-            x+=d[1]
-            lettre = board[y][x]
-            #print(lettre)
+	#tree = finder(board[(y, x)], tree)
+	#print(tree.lettre)
+	for d in directions:
+		if 0<=y+d[0]<=3 and 0<=x+d[1]<=3 and not board[y+d[0],x+d[1]] in passed:
+			y+=d[0]
+			x+=d[1]
+			lettre = board[y][x]
+			#print(lettre)
 
-            #print([branche.lettre for branche in tree.child])
-            if lettre in [branche.lettre for branche in tree.child]:
-                test = finder(lettre, tree) #board[(y, x)]
-                if test != False:
-                    passed.append(lettre)
-                    if test.word:
-                        print(test.get_word())
-                    #print(len(passed))
-                    #print(board, x, y)
-                    parser(y, x, board, test)
-                else:
-                    passed.pop()
+			#print([branche.lettre for branche in tree.child])
+			if lettre in [branche.lettre for branche in tree.child]:
+				test = finder(lettre, tree) #board[(y, x)]
+				if test != False:
+					passed.append(lettre)
+					if test.word:
+						print(test.get_word())
+					#print(len(passed))
+					#print(board, x, y)
+					parser(y, x, board, test)
+				else:
+					passed.pop()
 
 for y in range(4):
-    for x in range(4):
-        print('-', board[y,x])
-        passed = []
-        #parser(y, x, board, dictionnaire)
+	for x in range(4):
+		print('-', board[y,x])
+		passed = []
+		#parser(y, x, board, dictionnaire)
